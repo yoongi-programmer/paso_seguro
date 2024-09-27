4d2b168ff394061980fd2396d162e82c9245715a
GonzaloSSJ12 <sanchezgonzalo534@gmail.com>
2024-09-27 18:08:11 -0300

    Codigo modificado para que se conecte automaticamente al bt

--- app/src/main/java/com/app/pasoseguro/HomeFragment.kt
+++ app/src/main/java/com/app/pasoseguro/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.app.pasoseguro
 
+import android.R
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,12 +12,17 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import java.io.IOException
+import java.util.UUID
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -32,7 +39,7 @@ class HomeFragment : Fragment() {
     }
     private val enableBluetoothLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            //searchAndDisplayBluetoothDevices()
+            searchAndDisplayBluetoothDevice()
         } else {
             Snackbar.make(requireView(), "Bluetooth not enabled", Snackbar.LENGTH_SHORT).show()
         }
@@ -41,21 +48,23 @@ class HomeFragment : Fragment() {
     private var bluetoothSocket: BluetoothSocket? = null
     private lateinit var bluetoothManager: BluetoothManager
     private var isConnected: Boolean = false
-    private lateinit var devicesBluetooth: MutableList<BluetoothDevice>//lista mutable de objetos tipo bt_device
+    val targetDeviceName = "HC-05"
+    val targetMacAddress = "00:14:03:19:24:58"
+    private lateinit var deviceBluetooth: MutableList<BluetoothDevice>//lista mutable de objetos tipo bt_device
     private lateinit var bluetoothDevice: BluetoothDevice
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflar la interfaz para este fragmento
         _binding = FragmentHomeBinding.inflate(inflater,container,false)
 
         //SOLICITAR PERMISOS PARA UTILIZAR BLUETOOTH--------------------------------------
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {//Revisa version de android y solicita permisos de forma acorde
-            Log.d("homeFragment", "Requesting permissions for android 12+")
+            Log.d("HomeFragment", "Requesting permissions for android 12+")
             requestBluetoothPermissions()// Solicitar permisos de Bluetooth para Android 12+
         } else {
-            Log.d("homeFragment", "Requesting permissions for android 11-")
+            Log.d("HomeFragment", "Requesting permissions for android 11-")
             requestLegacyBluetoothPermissions()// Solicitar permisos de Bluetooth para Android 11 y anteriores
         }
 
@@ -81,6 +90,7 @@ class HomeFragment : Fragment() {
             //searchAndDisplayBluetoothDevices()
         }
     }
+    @RequiresApi(Build.VERSION_CODES.S)
     private fun requestBluetoothPermissions() {//Funcion para solicita permisos bt a android 12 y superiores
         val requiredPermissions = arrayOf(
             android.Manifest.permission.BLUETOOTH_CONNECT,
@@ -97,5 +107,50 @@ class HomeFragment : Fragment() {
             //searchAndDisplayBluetoothDevices()
         }
     }
+    private fun searchAndDisplayBluetoothDevice() {//Funcion para buscar y mostrar dispositivos BT
+        // Verificar si el Bluetooth está habilitado
+        if (bluetoothAdapter == null || !bluetoothAdapter!!.isEnabled) {// Verificar si el Bluetooth está habilitado
+            enableBluetoothLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))// Solicitar al usuario que habilite el Bluetooth
+        } else {
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices// Obtener la lista de dispositivos Bluetooth emparejados
+            pairedDevices?.forEach { device ->
+                if (device.name == targetDeviceName) {
+                    // Intentar conectarse automáticamente al dispositivo encontrado
+                    connectToDevice(device)
+                }
+            }
+            val device: BluetoothDevice = bluetoothAdapter!!.getRemoteDevice(targetMacAddress)
+            connectToDevice(device)
 
+        }
+    }
+    private fun connectToDevice(device: BluetoothDevice) {
+        Thread {
+            try {
+                val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+                Log.d("HomeFragment", "Creating a socket")
+                val socket = device.createRfcommSocketToServiceRecord(uuid)
+                bluetoothSocket = socket
+                bluetoothAdapter?.cancelDiscovery()
+                Log.d("HomeFragment", "Trying to connect")
+                socket.connect()
+
+                requireActivity().runOnUiThread {
+                    Log.d("HomeFragment", "Connected to device")
+                    Snackbar.make(requireView(), "Se conectó al dispositivo correctamente", Snackbar.LENGTH_SHORT).show()
+                    //startDataReceiving()
+                }
+            } catch (e: IOException) {
+                Log.e("HomeFragment", "Error connecting to device: ${e.message}")
+                requireActivity().runOnUiThread {
+                    Snackbar.make(requireView(), "Error al conectar al dispositivo", Snackbar.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Log.e("HomeFragment", "General error: ${e.message}")
+                requireActivity().runOnUiThread {
+                    Snackbar.make(requireView(), "Error desconocido", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }.start()
+    }
 }
