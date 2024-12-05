58b16d89aa5cab86724a9145499815f38a58e797
mel <melanieromero7111@gmail.com>
2024-12-04 22:07:38 -0300

    ultimos cambios: conectividad bluetooth y recepcion de datos

--- app/src/main/java/com/app/pasoseguro/HomeFragment.kt
+++ app/src/main/java/com/app/pasoseguro/HomeFragment.kt
@@ -1,34 +1,30 @@
 package com.app.pasoseguro
-
 import android.app.Activity
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.app.pasoseguro.databinding.FragmentHomeBinding
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.os.Build
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.app.pasoseguro.databinding.FragmentHomeBinding
 import com.google.android.material.snackbar.Snackbar
 import java.io.IOException
 import java.util.UUID
-import android.media.MediaPlayer
-import android.net.Uri
-import android.media.AudioManager
-import android.media.SoundPool
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -59,7 +55,7 @@ class HomeFragment : Fragment() {
     private lateinit var bluetoothManager: BluetoothManager
     private var isConnected: Boolean = false
     val targetDeviceName = "HC-05"
-    val targetMacAddress = "00:11:35:96:97:45"
+    val targetMacAddress = "98:D3:32:30:D4:5B"
     private lateinit var devicesBluetooth: MutableList<BluetoothDevice>//lista mutable de objetos tipo bt_device
     private lateinit var bluetoothDevice: BluetoothDevice
     var estadoSemaforo = 0
@@ -67,8 +63,18 @@ class HomeFragment : Fragment() {
     var estadoRojo = 0
     var estadoAmarillo = 0
     var estadoVerde = 0
+    @Volatile
+    var recibir = 0
+    var device : BluetoothDevice? = null
+    // Sincronizar el acceso a 'recibir'
+    private val recibirLock = Any()
+
     private var mediaPlayer : MediaPlayer? = null
     private val handler = Handler(Looper.getMainLooper())
+    private val dataReceiverHandler = Handler(Looper.getMainLooper())
+    private var isReceivingData = false
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -89,45 +95,27 @@ class HomeFragment : Fragment() {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 if (devicesBluetooth.isNotEmpty()) {
                     Log.d("homeFragment", "Selected device: ${devicesBluetooth[position].name}")
-                    val device = devicesBluetooth[position]
-                    connectToDevice(device)
+                    device = devicesBluetooth[position]
+                    connectToDevice(device!!)
                 }
             }
             override fun onNothingSelected(parent: AdapterView<*>) {
                 Snackbar.make(requireView(), "Seleccione un dispositivo", Snackbar.LENGTH_SHORT).show()
             }
         }
-        binding.btEscuchar.setOnClickListener{
-            semaforoActivo = true
-            iniciarSemaforo()
-        }
-        binding.btnPararSemaforo.setOnClickListener {
-            detenerSemaforo()
-        }
 
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             // Código que quieres ejecutar después del delay
-//            playAudio(R.raw.conexion)
-            isConnected = true
+            playAudio(R.raw.calle)
             handler.postDelayed({
-                // Código que quieres ejecutar después del delay
-//                playAudio(R.raw.calle)
-                handler.postDelayed({
-                    // Código que quieres ejecutar después del delay
-//                    playAudio(R.raw.cruce_dificil)
-
-                }, 4000) // 4000 milisegundos = 4 segundos
-            }, 4000)
+                playAudio(R.raw.cruce_dificil)
+                // Actualización de la variable sincronizada
+                synchronized (recibirLock) {
+                    recibir = 1
+                }
+            }, 4000) // 4000 milisegundos = 4 segundos
         }, 4000)
-//        Log.d("HomeFragment", "isConnected: $isConnected")
-//        updateUI()
-//        if (isConnected){
-//            updateUI()
-//        }else{
-//            updateUIWithDeafultValues()
-//        }
-
         return binding.root
     }
 
@@ -187,7 +175,6 @@ class HomeFragment : Fragment() {
             pairedDevices?.forEach { device ->
                 if (device.name == targetDeviceName) {
                     // Intentar conectarse automáticamente al dispositivo encontrado
-                    Log.d("HomeFragment","$device.name dispositivo")
                     connectToDevice(device)
                 }
             }
@@ -221,98 +208,42 @@ class HomeFragment : Fragment() {
         Thread {
             try {
                 val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-                Log.d("HomeFragment", "Creando un socket")
                 val socket = device.createRfcommSocketToServiceRecord(uuid)
-                bluetoothSocket = socket
-                bluetoothAdapter?.cancelDiscovery()
-                Log.d("HomeFragment", "Intentando conectar")
+                socket.connect()
+                bluetoothSocket = socket // Guardar el socket conectado
 
-                // Mostrar mensaje de conexión en progreso solo una vez
+                // Notificar que la conexión fue exitosa
                 requireActivity().runOnUiThread {
-                    binding.txtConectado.text = "Buscando dispositivo..."
+                    Log.d("Bluetooth", "Conectado al dispositivo")
+                    Snackbar.make(requireView(), "Conexión exitosa", Snackbar.LENGTH_SHORT).show()
                 }
-
-                socket.connect() // Intentar la conexión
-
-                // Si la conexión es exitosa
-                requireActivity().runOnUiThread {
-                    Log.d("HomeFragment", "Conectado al dispositivo")
-                    Snackbar.make(requireView(), "Se conectó al dispositivo correctamente", Snackbar.LENGTH_SHORT).show()
-                    binding.txtConectado.text = "Conectado"
-                    isConnected = true
-                    startDataReceiving() // Comenzar a recibir datos
-                }
-
+                Log.d("recibir","recibir es igual: $recibir")
+                // Iniciar la recepción de datos
+//                if(recibir == 1){
+                    Log.d("recibir","entro al recibir==1")
+                    playAudio(R.raw.conexion)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        startDataReceiving()
+                    }, 5000) // Espera 4000ms (4 segundos)
+//                }
             } catch (e: IOException) {
-                Log.i("HomeFragment", "No se encontraron dispositivos: ${e.message}")
                 requireActivity().runOnUiThread {
-                    // No mostrar el error si no se encuentra el dispositivo, solo continuar intentando
-                    //binding.txtConectado.text = "Buscando dispositivo..."
+                    Log.e("Bluetooth","Error al conectar o leer datos: " + e.message
+                    )
                 }
-                Thread.sleep(2000) // Esperar 2 segundos antes de reintentar
-
-            } catch (e: Exception) {
-                isConnected = false
-                Log.e("HomeFragment", "Error general: ${e.message}")
-                requireActivity().runOnUiThread {
-                    // Error inesperado, solo notificar si es necesario
-                    Snackbar.make(requireView(), "Error desconocido", Snackbar.LENGTH_SHORT).show()
-                }
-                Thread.sleep(2000) // Esperar 2 segundos antes de reintentar
             }
         }.start()
-    }
-
-    private fun startDataReceiving() {
-        Log.d("HomeFragment", "Recibiendo datos:")
-        val socket = bluetoothSocket
-        if (socket != null) {
-            val inputStream = socket.inputStream
-            val buffer = ByteArray(1024)
-            val stringBuilder = StringBuilder()
-            var bytes: Int
-
-            Thread {
-                while (true) {
-                    try {
-                        bytes = inputStream.read(buffer)
-                        val data = String(buffer, 0, bytes)
-                        stringBuilder.append(data)
-
-                        val lines = stringBuilder.split("\n")
-                        for (i in 0 until lines.size - 1) {
-                            val line = lines[i].trim()
-                            if (line.isNotEmpty()) {
-                                Log.d("HomeFragment", "Received data: $line")
-                                parseData(line)
-                                updateUI()
-                            }
-                        }
-                        stringBuilder.clear()
-                        stringBuilder.append(lines.last())
-                    } catch (e: IOException) {
-                        Log.e("HomeFragment", "Error reading data: ${e.message}")
-                        break
-                    }
-                }
-            }.start()
-        } else {
-            Log.e("HomeFragment", "BluetoothSocket is null")
-            Snackbar.make(requireView(), "Error al conectar al dispositivo", Snackbar.LENGTH_SHORT)
-                .show()
-        }
     }
 
     private fun parseData(data: String) {
         Log.d("HomeFragment", "Parsing data: $data")
         val parts = data.split("|")
-        Log.d("HomeFragment","Tamaño de partes: ${parts.size}")
         if (parts.size == 3) {
             try {
                 // Parsear todos los datos a el formato correspondiente
-                estadoVerde = parts[0].toInt()
+                estadoRojo = parts[0].toInt()
                 estadoAmarillo = parts[1].toInt()
-                estadoRojo = parts[2].toInt()
+                estadoVerde = parts[2].toInt()
             } catch (e: NumberFormatException) {
                 Log.e("homeFragment", "Error parsing data: ${e.message}")
             }
@@ -322,65 +253,100 @@ class HomeFragment : Fragment() {
 
     }
 
-    private fun updateUI() {
-        Log.d("HomeFragment", "Actualizando UI")
-        binding.txtConectado.text = "Conectado"
-        Log.d("HomeFragment","estado verde: $estadoVerde")
-        Log.d("HomeFragment","estado amarillo: $estadoAmarillo")
-        Log.d("HomeFragment","estado rojo: $estadoRojo")
+    private fun startDataReceiving() {
+        Log.d("HomeFragment", "Recibiendo datos:")
+        val socket = bluetoothSocket
+        if (socket != null) {
+            val inputStream = socket.inputStream
+            val buffer = ByteArray(1024)
+            val stringBuilder = StringBuilder()
 
-        if (estadoVerde == 1) {
-            playAudio2(R.raw.semaforo_verde)
-        }
-        else if (estadoAmarillo == 1) {
-            playAudio2(R.raw.semaforo_amarillo)
-        }
-        else if (estadoRojo == 1) {
-            playAudio2(R.raw.semaforo_rojo)
-        }
+            // Evitar ciclo infinito y controlar la recepción de datos con Handler
+            isReceivingData = true
+            dataReceiverHandler.post(object : Runnable {
+                override fun run() {
+                    if (isReceivingData) {
+                        try {
+                            val bytes = inputStream.read(buffer)
+                            val data = String(buffer, 0, bytes)
+                            stringBuilder.append(data)
 
+                            val lines = stringBuilder.split("\n")
+                            for (i in 0 until lines.size - 1) {
+                                val line = lines[i].trim()
+                                if (line.isNotEmpty()) {
+                                    Log.d("HomeFragment", "Received data: $line")
+                                    parseData(line)
+                                    cambiarColor(binding.luzVerde, R.color.verde_off)
+                                    cambiarColor(binding.luzAmarilla, R.color.amarillo_off)
+                                    cambiarColor(binding.luzRoja, R.color.rojo_off)
+                                    updateUI()
+                                }
+                            }
+                            stringBuilder.clear()
+                            stringBuilder.append(lines.last()) // Mantener la última línea incompleta
 
+                        } catch (e: IOException) {
+                            Log.e("HomeFragment", "Error leyendo datos: ${e.message}")
+                            // Intentar reconectar si el socket se cierra
+                            handleBluetoothDisconnection()
+                        }
+
+                        // Reintentar recibir datos después de un breve retraso (1 segundo)
+                        dataReceiverHandler.postDelayed(this, 1000) // 1 segundo de retraso
+                    }
+                }
+            })
+        } else {
+            Log.e("HomeFragment", "BluetoothSocket es nulo")
+            Snackbar.make(requireView(), "Error al conectar al dispositivo", Snackbar.LENGTH_SHORT).show()
+        }
     }
+
+    // Función para manejar la desconexión del Bluetooth y reconectar
+    private fun handleBluetoothDisconnection() {
+        Log.e("HomeFragment", "Conexión Bluetooth perdida. Intentando reconectar...")
+        try {
+            bluetoothSocket?.close()
+            // Intenta reconectar, por ejemplo, al Bluetooth nuevamente
+            device?.let { connectToDevice(it) } // Implementa la lógica de reconexión aquí
+        } catch (e: IOException) {
+            Log.e("HomeFragment", "Error al cerrar el socket: ${e.message}")
+        }
+    }
+
+    // Función para detener la recepción de datos
+    fun stopDataReceiving() {
+        isReceivingData = false
+        dataReceiverHandler.removeCallbacksAndMessages(null)
+    }
+
+
+    private fun updateUI() {
+        requireActivity().runOnUiThread {
+            Log.d("HomeFragment", "Actualizando UI")
+            binding.txtConectado.text = "Conectado"
+
+            when {
+                estadoVerde == 1 -> {
+                    playAudio(R.raw.semaforo_verde)
+                    cambiarColor(binding.luzVerde, R.color.verde_on)
+                }
+                estadoAmarillo == 1 -> {
+                    playAudio(R.raw.semaforo_amarillo)
+                    cambiarColor(binding.luzAmarilla, R.color.amarillo_on)
+                }
+                estadoRojo == 1 -> {
+                    playAudio(R.raw.semaforo_rojo)
+                    cambiarColor(binding.luzRoja, R.color.rojo_on)
+                }
+            }
+        }
+    }
+
     private fun cambiarColor(view: View, color: Int) {
         val background = view.background.mutate() // Asegúrate de no modificar el drawable globalmente
         background.setTint(ContextCompat.getColor(view.context, color)) // Cambia el colorthis, color)) // Cambia el color
-    }
-    private fun iniciarSemaforo() {
-        if (semaforoActivo) {
-            // Luz roja
-            cambiarColor(binding.luzRoja, R.color.rojo_on)
-            playAudio(R.raw.semaforo_rojo)
-            handler.postDelayed({
-                cambiarColor(binding.luzRoja, R.color.rojo_off)
-                // Luz amarilla
-                cambiarColor(binding.luzAmarilla, R.color.amarillo_on)
-                playAudio(R.raw.semaforo_amarillo)
-                handler.postDelayed({
-                    cambiarColor(binding.luzAmarilla, R.color.amarillo_off)
-                    // Luz verde
-                    cambiarColor(binding.luzVerde, R.color.verde_on)
-                    playAudio(R.raw.semaforo_verde)
-                    handler.postDelayed({
-                        cambiarColor(binding.luzVerde, R.color.verde_off)
-                        iniciarSemaforo() // Reiniciar ciclo
-                    }, 9500) // 3 segundos para verde
-                }, 6000) // 1 segundo para amarillo
-            }, 8000) // 5 segundos para rojo
-        }
-    }
-    private fun detenerSemaforo() {
-        // Luz roja
-        cambiarColor(binding.luzRoja, R.color.rojo_off)
-        // Luz amarilla
-        cambiarColor(binding.luzAmarilla, R.color.amarillo_off)
-        // Luz verde
-        cambiarColor(binding.luzVerde, R.color.verde_off)
-        semaforoActivo = false
-    }
-    private fun updateUIWithDeafultValues(){
-        Log.d("HomeFragment", "Desconectado")
-        binding.txtConectado.text = "Desconectado"
-
     }
     private fun playAudio(audioResID: Int) {
         // Verificar si ya hay un MediaPlayer reproduciendo
@@ -414,9 +380,6 @@ class HomeFragment : Fragment() {
                     mediaPlayer = null // Liberar la referencia del MediaPlayer
                     Log.d("MediaPlayer", "MediaPlayer liberado.")
 
-                    // Ahora puedes reproducir el siguiente audio si es necesario
-                    // (En este caso no estamos llamando a otro audio directamente,
-                    // pero puedes hacerlo si tienes una cola de reproducción).
                 }
 
             } ?: run {
@@ -427,24 +390,8 @@ class HomeFragment : Fragment() {
             Log.e("MediaPlayer", "Excepción al reproducir audio: ${e.message}")
         }
     }
-    private fun playAudio2(audioResID: Int) {
-        try {
-            mediaPlayer = MediaPlayer.create(requireContext(), audioResID)
-            mediaPlayer?.let {
-                it.setOnPreparedListener { player -> player.start() }
-                it.setOnErrorListener { player, what, extra ->
-                    Log.e("MediaPlayer", "Error al preparar el MediaPlayer: what=$what, extra=$extra")
-                    true // Indica que el error fue manejado
-                }
-            } ?: run {
-                Log.e("MediaPlayer", "Error al crear el MediaPlayer")
-            }
-        } catch (e: Exception) {
-            Log.e("MediaPlayer", "Excepción al reproducir audio: ${e.message}")
-        }
-    }
 
-        override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
         if (bluetoothSocket != null) {
